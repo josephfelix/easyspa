280b962cad8454a78d2f47d651916c39b299b675
Joseph Felix <joseph0xfff@gmail.com>
2015-12-14 13:22:07 -0200

    Alterando URL e removendo log de erros

--- Site/application/config/config.php
+++ Site/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/easyspa/';
+$config['base_url']	= 'http://easyspa.dev/';
 
 /*
 |--------------------------------------------------------------------------
